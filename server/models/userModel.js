595f002ad1fa4b72c7dd06888ca4c5e82ef4bd68
khyatae <khyathi2003kotipalli@gmail.com>
2024-04-25 00:23:21 +0530

    added user model

---
+++ server/models/userModel.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+
+const userSchema=new mongoose.Schema('Users',{
+    name:{
+        type:String,
+        required:[true,"name is required"]
+    },
+    email:{
+         type:email,
+         required:[true,"email is required"],
+         validate:validator.isEmail,
+         unique:true
+    },
+    number:{
+        type:Number,
+        required:[true,"number is required"],
+        unique:true,
+    },
+    dob:{
+        type:Date,
+        required:[true,"date of birth is required"],
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+        minlength: [6, "Password length should be greater than 6 character"],
+        select: true,
+    },
+    accountType: { type: String, default: "volunteer" },
+    contact: { type: String },
+    location: { type: String },
+    profileUrl: { type: String },
+    cvUrl: { type: String },
+    jobTitle: { type: String },
+    about: { type: String },
+   },
+    { timestamps: true }
+);
+
+userSchema.pre('save',async function(){
+    if (!this.isModified) return;
+    const salt=await bcrypt.genSalt(10);
+    this.password=await brcypt.hash(this.password,salt);
+})
+
+userSchema.methods.comparePassword=async function(userPassword){
+    const conformPassword=await bcrypt.compare(userPassword,this.password);
+    return conformPassword;
+}
+
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+};
+
+const Users=mongoose.model('Users',userSchema);
+
+export default Users;
