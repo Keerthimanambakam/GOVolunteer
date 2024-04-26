340e871cebe1ff406d7d65e898af90eac35b5e2f
khyatae <khyathi2003kotipalli@gmail.com>
2024-04-27 04:19:49 +0530

    updated companies profile

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -3,20 +3,7 @@ import mongoose from "mongoose";
 const dbConnection=async()=>{
     try{
         const dbConnection=await mongoose.connect(process.env.MONGODB_URL);
-<<<<<<< HEAD
-=======
-=======
-<<<<<<< HEAD
-        const dbConnection=await mongoose.connect(process.env.MONGODB_URL);
-=======
-        const dbConnection=await mongoose.connect(process.env.MONGODB_URL,{
-            useNewUrlParser: true,
-        
 
-    });
->>>>>>> b9e5b0a4f244adc51b07bb6efeb19d46de0d84ef
->>>>>>> 4a0f3082c605446168e8e32dd0933022f71a9b01
->>>>>>> dc7783114bd63a9f12ec6b2fe00709e181a05a9a
         console.log("DB connected successfully")
     }
     catch(error)
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -47,23 +47,10 @@ userSchema.pre('save',async function(){
 })
 
 userSchema.methods.comparePassword=async function(userPassword){
-<<<<<<< HEAD
+
+
     const confirmPassword=await bcrypt.compare(userPassword,this.password);
     return confirmPassword;
-=======
-<<<<<<< HEAD
-    const conformPassword=await bcrypt.compare(userPassword,this.password);
-    return conformPassword;
-=======
-<<<<<<< HEAD
-    const conformPassword=await bcrypt.compare(userPassword,this.password);
-    return conformPassword;
-=======
-    const confirmPassword=await bcrypt.compare(userPassword,this.password);
-    return confirmPassword;
->>>>>>> b9e5b0a4f244adc51b07bb6efeb19d46de0d84ef
->>>>>>> 4a0f3082c605446168e8e32dd0933022f71a9b01
->>>>>>> dc7783114bd63a9f12ec6b2fe00709e181a05a9a
 }
 
 userSchema.methods.createJWT = function () {
