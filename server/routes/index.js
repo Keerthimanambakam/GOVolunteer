92b048cde4c6eaaf1efab538ce0ae26ac1a75795
Keerthimanambakam <keerthi99055@gmail.com>
2024-04-26 22:28:41 +0530

    updated user controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,12 +8,7 @@ const router = express.Router();
 
 const path = "/api/";
 
-<<<<<<< HEAD
 router.use(path+"auth", authRoute); 
 router.use(path+"user", userRoute);
-=======
-router.use(`${path}auth`, authRoute); 
-
->>>>>>> f2311a472c039aa891209fbcd566aa65d1b48518
 
 export default router;
