0d253245e5987c0bd48407ee68e4c51e94d78dc9
khyatae <khyathi2003kotipalli@gmail.com>
2024-04-26 14:33:06 +0530

    added upload oppurtunity page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -8,5 +8,7 @@ import Background from "./Background";
 import List from "./List";
 import JobCard from "./JobCard";
 import CompanyCard from "./CompanyCard";
+import JobTypes from "./JobTypes";
+import Loading from "./Loading";
 
-export {Footer,Navbar,CustomButton,MenuList,SignUp,Input,Background,List,JobCard,CompanyCard}
+export {Footer,Navbar,CustomButton,MenuList,SignUp,Input,Background,List,JobCard,CompanyCard,JobTypes,Loading}
